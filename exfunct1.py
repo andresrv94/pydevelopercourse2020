03e554c15b6f963c6cf78074c4f9cc75afd5220f
Andres R. Villarroel <andresrvillarroel@gmail.com>
2019-12-26 18:25:44 -0300

    Exercise finished

--- exfunct1.py
+++ exfunct1.py
@@ -1,6 +1,12 @@
 #!/bin/python3
 
 def highest_even(li):
-    pass
+    even_numbers=[]
+    for i in li:
+        if i%2 == 0:
+            even_numbers.append(i)
+    return(max(even_numbers))
 
-print(highest_even([10,2,3,4,8,11]))
+
+
+print(highest_even([10,2,3,4,8,11,10000,10003]))
